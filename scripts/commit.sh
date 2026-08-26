2a16cd9c9d92c063e041bcbd109db23fbb7a0e76
Gui Reis <gui.sreis25@gmail.com>
2026-08-26 01:56:19 -0300

    Segregando commits

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ -n "${NOTICE:-}" ]; then
-  echo "::notice title=${NOTICE_TITLE:-finish}::${NOTICE}"
-fi
-
 git config user.name "github-actions[bot]"
 git config user.email "github-actions[bot]@users.noreply.github.com"
 git add -A
